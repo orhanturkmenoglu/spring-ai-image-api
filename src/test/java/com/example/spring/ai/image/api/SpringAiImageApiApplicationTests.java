2be825a0a461add86e1b6a8dae4d00345cb48be9
orhan <orhantrkmn749@gmail.com>
2025-07-02 12:53:54 +0300

    initial commit

---
+++ src/test/java/com/example/spring/ai/image/api/SpringAiImageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.ai.image.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
